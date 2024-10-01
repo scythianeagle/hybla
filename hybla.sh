4f8421e0c6340ae54cbbba9c38924bd5b58a0d79
scythianeagle <132835586+scythianeagle@users.noreply.github.com>
2024-10-01 18:29:19 +0330

    Create hybla.sh

---
+++ hybla.sh
@@ -0,0 +1,6 @@
+echo "Running Hybla script..."
+            sleep 2
+            curl -fsSL https://raw.githubusercontent.com/MrAminiDev/NetOptix/main/scripts/hybla.sh -o /tmp/hybla.sh
+            bash /tmp/hybla.sh
+            rm /tmp/hybla.sh
+          
